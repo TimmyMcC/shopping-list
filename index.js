23fbde87daec05d4b0c267205d1b6105291a78ed
TimmyMcC <53845486+TimmyMcC@users.noreply.github.com>
2019-09-03 15:40:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 $(function() {
-    $('#s-shopping-list-form').submit(function() {
-        event.preventDefault();
-        const shoppingList = $('.shopping-list');
+
+// here i want to add a <li> complete with buttons
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    $('ul').append($(
+      '<li><span class="shopping-item">' +
+      ${text: $('#js-shopping-entry').val()} +
+      '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li'
+    ));
+  });
+
+// here i want to be able to check or uncheck (strikethrough or un-strikethrough) the list item based on clicking the 'check' button
+  $('.shopping-item-toggle').click(function() {
+    const checked = $(event.currentTarget);
+    checked.toggleClass('shopping-item__checked');
+  });
+
+// here i want to be able to remove a <li> by clicking the 'delete' button
+  $('.shopping-item-delete').click(function() {
+    const deleted = $(event.currentTarget);
+    deleted.remove('???');
+  });
+})
